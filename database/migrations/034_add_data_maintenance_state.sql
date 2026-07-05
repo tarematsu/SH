53c7dcff89bbd90dd28053a38243172847833f90
tarematsu <yuukiar0202@gmail.com>
2026-07-05 09:50:46 +0900

    Remove conflicting maintenance migration number

--- database/migrations/034_add_data_maintenance_state.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS sh_data_maintenance_state (
-  id TEXT PRIMARY KEY,
-  last_rollup_key TEXT,
-  last_cleanup_at INTEGER NOT NULL DEFAULT 0,
-  updated_at INTEGER NOT NULL
-);
