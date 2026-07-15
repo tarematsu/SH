19432e459c1ffe193d7fbaaa64382b2243b663f7
tarematsu <yuukiar0202@gmail.com>
2026-07-16 04:12:20 +0900

    Lazy load minute monitoring API

--- worker/src/minute-production-entry.js
+++ worker/src/minute-production-entry.js
@@ -1,5 +1,6 @@
 import minuteApp from './minute-entry.js';
-import { LATEST_MINUTE_FACTS_PATH } from './minute-facts-api.js';
+
+const LATEST_MINUTE_FACTS_PATH = '/api/minute-facts/latest';
 
 export async function runMinuteFetch(request, env, ctx, dependencies = {}) {
   const pathname = new URL(request.url).pathname;
