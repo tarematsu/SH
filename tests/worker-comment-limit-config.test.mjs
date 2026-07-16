0a76a53ded85b8ba2bb29fad751d511e7143a789
tarematsu <yuukiar0202@gmail.com>
2026-07-17 01:15:45 +0900

    Read comment limit from comments Worker config

--- tests/worker-comment-limit-config.test.mjs
+++ tests/worker-comment-limit-config.test.mjs
@@ -2,10 +2,10 @@ import test from 'node:test';
 import assert from 'node:assert/strict';
 import { readFile } from 'node:fs/promises';
 
-const config = JSON.parse(await readFile(new URL('../worker/wrangler.jsonc', import.meta.url), 'utf8'));
+const commentsConfig = JSON.parse(await readFile(new URL('../worker/wrangler.comments.jsonc', import.meta.url), 'utf8'));
 const otherConfig = JSON.parse(await readFile(new URL('../worker/wrangler.other.jsonc', import.meta.url), 'utf8'));
 
 test('normal and solo comment collection are configured for 50 items', () => {
-  assert.equal(config.vars.CHAT_LIMIT, 50);
+  assert.equal(commentsConfig.vars.CHAT_LIMIT, 50);
   assert.equal(otherConfig.vars.SOLO_CHAT_LIMIT, 50);
 });
