639d5ab91e7492ac0022da9506453ffd9195d9fe
tarematsu <yuukiar0202@gmail.com>
2026-07-17 06:21:58 +0900

    Expose lightweight other scheduler compatibility

--- worker/src/other-entry.js
+++ worker/src/other-entry.js
@@ -1,4 +1,12 @@
 import './fetch-guard.js';
 
 export * from './other-monitor-entry.js';
+export {
+  OTHER_WORKER_CRON,
+  otherProductionTask,
+  otherStaggerApplies,
+  runOfficialNewsWithReconcile,
+  runOtherCron,
+  runOtherScheduled,
+} from './other-entry-compat.js';
 export { default } from './other-monitor-entry.js';
