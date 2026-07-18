3c8434f549b1cc4c80e55a41b7a47eb62ebf8922
tarematsu <yuukiar0202@gmail.com>
2026-07-19 07:52:45 +0900

    Cover latest queue snapshot reads

--- database/buddies-migrations/007_d1_budget_indexes.sql
+++ database/buddies-migrations/007_d1_budget_indexes.sql
@@ -3,3 +3,6 @@ ON sh_channel_snapshots(observed_at, id);
 
 CREATE INDEX IF NOT EXISTS idx_sh_queue_items_start_station
 ON sh_queue_items(start_time, station_id);
+
+CREATE INDEX IF NOT EXISTS idx_sh_queue_snapshots_station_observed
+ON sh_queue_snapshots(station_id, observed_at DESC, id DESC);
