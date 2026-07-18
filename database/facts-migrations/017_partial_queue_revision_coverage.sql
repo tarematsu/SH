96d8ecac4b48e5f9e1bf963b41304fbf036f41ab
tarematsu <yuukiar0202@gmail.com>
2026-07-18 10:34:05 +0900

    Track partial queue revision coverage

---
+++ database/facts-migrations/017_partial_queue_revision_coverage.sql
@@ -0,0 +1,9 @@
+ALTER TABLE sh_queue_revisions ADD COLUMN total_item_count INTEGER;
+ALTER TABLE sh_queue_revisions ADD COLUMN coverage_complete INTEGER NOT NULL DEFAULT 1;
+
+UPDATE sh_queue_revisions
+SET total_item_count=item_count
+WHERE total_item_count IS NULL;
+
+CREATE INDEX IF NOT EXISTS idx_sh_queue_revisions_coverage
+  ON sh_queue_revisions(channel_id,coverage_complete,effective_at DESC);
