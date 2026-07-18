d6e4b9e935444f314a55897650467b657e6d2dff
tarematsu <yuukiar0202@gmail.com>
2026-07-19 02:19:36 +0900

    Remove redundant buddies monitor wrapper

--- worker/src/raw-collector-optimized-entry.js
+++
@@ -1,9 +0,0 @@
-import { collectRawChannel } from './raw-collector-entry.js';
-
-const EMPTY_DEPENDENCIES = Object.freeze({});
-
-export default {
-  scheduled(_controller, env, ctx) {
-    ctx.waitUntil(collectRawChannel(env, EMPTY_DEPENDENCIES));
-  },
-};
