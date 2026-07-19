f9b4921fbabe275a38b7544bf5d13c03d3b9b191
tarematsu <yuukiar0202@gmail.com>
2026-07-20 06:36:45 +0900

    Expose the consolidated one-minute monitor cron

--- worker/src/other-entry.js
+++ worker/src/other-entry.js
@@ -2,4 +2,5 @@ import './fetch-guard.js';
 
 export * from './other-monitor-entry.js';
 export * from './other-entry-compat.js';
+export { CONSOLIDATED_MONITOR_CRON as OTHER_WORKER_CRON } from './consolidated-monitor-entry.js';
 export { default } from './consolidated-monitor-entry.js';
