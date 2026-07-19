843e0aa8018158dda15d8ed2edfe06423ed38d6c
tarematsu <yuukiar0202@gmail.com>
2026-07-19 10:21:04 +0900

    Remove unused rebuild wrapper entry

--- worker/src/minute-rebuild-seek-entry.js
+++
@@ -1,8 +0,0 @@
-import { withBackfillCursorSeek } from './backfill-cursor-seek.js';
-import rebuildWorker from './minute-rebuild-maintenance-entry.js';
-
-export default {
-  queue(batch, env, ctx) {
-    return rebuildWorker.queue(batch, withBackfillCursorSeek(env), ctx);
-  },
-};
