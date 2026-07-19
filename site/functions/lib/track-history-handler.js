701e28b3b405a0761fc61e25c84bcded73de628f
tarematsu <yuukiar0202@gmail.com>
2026-07-19 11:24:28 +0900

    Serve track history through Apple-free SQL adapter

--- site/functions/lib/track-history-handler.js
+++ site/functions/lib/track-history-handler.js
@@ -1,1 +1,25 @@
-export * from './track-history-restored-handler.js';
+import {
+  handleTrackHistory as handleRestoredTrackHistory,
+  loadTrackHistoryData as loadRestoredTrackHistoryData,
+  TRACK_HISTORY_GRACE_MS,
+  TRACK_HISTORY_SQL as RESTORED_TRACK_HISTORY_SQL,
+} from './track-history-restored-handler.js';
+import {
+  appleMusicFreeTrackHistoryDatabase,
+  withAppleMusicFreeTrackHistoryEnv,
+  withoutAppleMusicTrackHistorySql,
+} from './apple-music-track-history-sql.js';
+
+export { TRACK_HISTORY_GRACE_MS };
+export const TRACK_HISTORY_SQL = withoutAppleMusicTrackHistorySql(RESTORED_TRACK_HISTORY_SQL);
+
+export function loadTrackHistoryData(db, ...args) {
+  return loadRestoredTrackHistoryData(appleMusicFreeTrackHistoryDatabase(db), ...args);
+}
+
+export function handleTrackHistory(context) {
+  return handleRestoredTrackHistory({
+    ...context,
+    env: withAppleMusicFreeTrackHistoryEnv(context?.env),
+  });
+}
