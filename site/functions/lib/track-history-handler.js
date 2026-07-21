3fea4e70e891ea83d7392bba458ffc93b4df0732
tarematsu <yuukiar0202@gmail.com>
2026-07-21 12:28:15 +0900

    Remove Apple Music track history adapter

--- site/functions/lib/track-history-handler.js
+++ site/functions/lib/track-history-handler.js
@@ -1,25 +1,6 @@
-import {
-  handleTrackHistory as handleRestoredTrackHistory,
-  loadTrackHistoryData as loadRestoredTrackHistoryData,
+export {
+  handleTrackHistory,
+  loadTrackHistoryData,
   TRACK_HISTORY_GRACE_MS,
-  TRACK_HISTORY_SQL as RESTORED_TRACK_HISTORY_SQL,
+  TRACK_HISTORY_SQL,
 } from './track-history-restored-handler.js';
-import {
-  appleMusicFreeTrackHistoryDatabase,
-  withAppleMusicFreeTrackHistoryEnv,
-  withoutAppleMusicTrackHistorySql,
-} from './apple-music-track-history-sql.js';
-
-export { TRACK_HISTORY_GRACE_MS };
-export const TRACK_HISTORY_SQL = withoutAppleMusicTrackHistorySql(RESTORED_TRACK_HISTORY_SQL);
-
-export function loadTrackHistoryData(db, ...args) {
-  return loadRestoredTrackHistoryData(appleMusicFreeTrackHistoryDatabase(db), ...args);
-}
-
-export function handleTrackHistory(context) {
-  return handleRestoredTrackHistory({
-    ...context,
-    env: withAppleMusicFreeTrackHistoryEnv(context?.env),
-  });
-}
